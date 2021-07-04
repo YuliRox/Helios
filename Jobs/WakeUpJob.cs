ef13eb696ae57d6facf8ce027814d51a9cc97df4
Xemrox <xemrox@users.noreply.github.com>
2021-07-05 00:57:06 +0200

    added quartz job scheduling

---
+++ Jobs/WakeUpJob.cs
@@ -0,0 +1,69 @@
+
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using Quartz;
+
+namespace Helios.Jobs
+{
+    public class WakeUpJob : IJob
+    {
+        private static async Task SendPercentage(IMqttClient mqttClient, string topic, int percentage) {
+            await mqttClient.PublishAsync(new MqttApplicationMessage() {
+                Topic = topic,
+                Payload = Encoding.UTF8.GetBytes(percentage.ToString())
+            });
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                var dimmerPercentageCommandTopic = context.MergedJobDataMap.GetString("Helios:PercentageTopic");
+                var dimmerOnOffCommandTopic = context.MergedJobDataMap.GetString("Helios:OnOffTopic");
+
+                var mqttClient = new MqttFactory().CreateMqttClient();
+
+                var mqttClientOptionsBuilder = new MqttClientOptionsBuilder();
+                var mqttClientOptions = mqttClientOptionsBuilder
+                    .WithClientId(context.MergedJobDataMap.GetString("Mqtt:ClientId"))
+                    .WithTcpServer(
+                        context.MergedJobDataMap.GetString("Mqtt:Server"),
+                        context.MergedJobDataMap.GetInt("Mqtt:Port"))
+                    .WithCredentials(
+                        context.MergedJobDataMap.GetString("Mqtt:Username"),
+                        context.MergedJobDataMap.GetString("Mqtt:Password"))
+                    .Build();
+
+                await mqttClient.ConnectAsync(mqttClientOptions);
+
+                await mqttClient.PublishAsync(new MqttApplicationMessage() {
+                    Topic = dimmerOnOffCommandTopic,
+                    Payload = Encoding.UTF8.GetBytes("ON")
+                });
+
+                await SendPercentage(mqttClient, dimmerPercentageCommandTopic, 0);
+
+                var percentage = 20;
+                while(percentage < 100) {
+
+                    await SendPercentage(mqttClient, dimmerPercentageCommandTopic, percentage);
+
+                    percentage++;
+                    await Task.Delay(15000);
+                }
+
+                await mqttClient.DisconnectAsync();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Helios.Extensions;
+using Helios.Jobs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Quartz;
 
 namespace Helios
 {
@@ -26,6 +28,7 @@ namespace Helios
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
             services.AddMqttClientServiceWithConfig((configBuilder) =>
             {
                 configBuilder
@@ -45,6 +48,42 @@ namespace Helios
                 heliosOptions.DimmerOnOffCommandTopic = Configuration["Mqtt:Topics:DimmerOnOffCommand"];
                 heliosOptions.DimmerPercentageStatusTopic = Configuration["Mqtt:Topics:DimmerPercentageStatus"];
                 heliosOptions.DimmerPercentageCommandTopic = Configuration["Mqtt:Topics:DimmerPercentageCommand"];
+            });
+
+            services.AddQuartz(q =>
+            {
+                q.AddJob<WakeUpJob>(j => j
+                    .WithIdentity("wakeUp")
+                    .UsingJobData("Mqtt:ClientId", Configuration.GetValue<string>("Mqtt:ClientId", "HELIOS-Daemon"))
+                    .UsingJobData("Mqtt:Server", Configuration["Mqtt:Server"])
+                    .UsingJobData("Mqtt:Port", Configuration.GetValue<int>("Mqtt:Port", 1883))
+                    .UsingJobData("Mqtt:Username", Configuration["Mqtt:Username"])
+                    .UsingJobData("Mqtt:Password", Configuration["Mqtt:Password"])
+                    .UsingJobData("Helios:PercentageTopic", Configuration["Mqtt:Topics:DimmerPercentageCommand"])
+                    .UsingJobData("Helios:OnOffTopic", Configuration["Mqtt:Topics:DimmerOnOffCommand"])
+                );
+
+                q.AddTrigger(t =>
+                    t.WithIdentity("wakeUp")
+                    .ForJob("wakeUp")
+                    .StartAt(new DateTimeOffset(2021, 7, 5, 7, 40, 0, TimeSpan.Zero))
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInHours(24)
+                        .WithRepeatCount(5)
+                    )
+                );
+
+                /*q.AddTrigger(t =>
+                    t.WithIdentity("wakeUp")
+                    .ForJob("wakeUp")
+                    .StartNow()
+                );*/
+            });
+
+            services.AddQuartzServer(options =>
+            {
+                // when shutting down we want jobs to complete gracefully
+                options.WaitForJobsToComplete = true;
             });
         }
 
