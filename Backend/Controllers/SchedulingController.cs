4e7312bd5736a9998a66db8039b14c6716a6a414
Xemrox <xemrox@users.noreply.github.com>
2021-10-03 18:40:51 +0200

    Added ServiceEndpoints for RunNow and GetScheduledEvents

--- Backend/Controllers/SchedulingController.cs
+++ Backend/Controllers/SchedulingController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Helios.DTO;
+using Quartz;
+using System.Linq;
+using Quartz.Impl.Matchers;
 
 namespace Helios.Controllers
 {
@@ -10,11 +13,30 @@ namespace Helios.Controllers
     [Route("api/[controller]")]
     public class SchedulingController : ControllerBase
     {
+        public ISchedulerFactory SchedulerFactory { get; }
+
+        public SchedulingController(ISchedulerFactory schedulerFactory)
+        {
+            SchedulerFactory = schedulerFactory;
+        }
 
         // GET: api/Scheduling
-        [HttpGet]
+        [HttpGet("GetScheduledEvents")]
         public async Task<IEnumerable<ScheduledEventDTO>> GetScheduledEvents()
         {
+            var schedulers = await SchedulerFactory.GetAllSchedulers();
+            var scheduler = schedulers.FirstOrDefault();
+            if (scheduler == null)
+                return Enumerable.Empty<ScheduledEventDTO>();
+
+            //var groups = await scheduler.GetJobGroupNames();
+            var keys = await scheduler.GetJobKeys(GroupMatcher<JobKey>.AnyGroup());
+            var jobKey = keys.FirstOrDefault();
+            if (jobKey == null)
+                return Enumerable.Empty<ScheduledEventDTO>();
+
+            var triggers = await scheduler.GetTriggersOfJob(jobKey);
+
             var scheduledEvents = new ScheduledEventDTO()
             {
                 Weekday = Enum.Weekday.Monday,
@@ -23,6 +45,18 @@ namespace Helios.Controllers
             };
 
             return new[] { scheduledEvents };
+        }
+
+        [HttpPost("RunNow")]
+        public async Task RunNow(string jobName)
+        {
+            var schedulers = await SchedulerFactory.GetAllSchedulers();
+            var scheduler = schedulers.FirstOrDefault();
+            if (scheduler == null)
+                return;
+
+            var jobKey = new JobKey(jobName);
+            await scheduler.TriggerJob(jobKey);
         }
     }
 }
