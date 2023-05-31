847219cc3b7b6da429a56ae7de3a47223d6de065
Sandra Schuhmacher <coding@voiding-warranties.de>
2023-05-31 23:31:19 +0200

    move Dockerfile to .NET 7

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-REGISTRY=yulirox-pi:5000
-docker buildx build --platform=linux/arm64  --build-arg DOTNET_ARCH="arm64" --build-arg BASE_IMAGE_ARCH="-arm64v8" -t $REGISTRY/helios:arm64-v1.0 --push .
+REGISTRY=registry.chaos-collision.de
+
+echo "Logging in to registry $REGISTRY"
+
+if [ -z "$REG_PW" ]; then
+	echo "ENV variable REG_PW must be set with registry password"
+	exit 1
+fi
+
+if [ -z "$REG_USER" ]; then
+	echo "ENV variable REG_USER must be set with registry username"
+	exit 1
+fi
+
+docker login $REGISTRY -p $REG_PW -u $REG_USER
+docker buildx build --platform=linux/arm64  --build-arg DOTNET_ARCH="arm64" --build-arg BASE_IMAGE_ARCH="-arm64v8" -t $REGISTRY/helios:arm64-v1.0 -t $REGISTRY/helios:latest --push .
